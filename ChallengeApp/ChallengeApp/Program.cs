f5f27e85ae35d7dfce2423d9022fb1198fa14c38
Bartek Wieczorek <wieczorek.bartlomiej@wp.pl>
2023-01-27 07:37:46 +0100

    Pierwsza aplikacjia

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, give me your name!");
+string name = Console.ReadLine();
+Console.WriteLine($"Welcome: " + name);
