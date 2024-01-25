b68118e0fcc7be49ea64d45734a1139bca7503d8
Bartek Wieczorek <wieczorek.bartlomiej@wp.pl>
2024-01-25 22:52:15 +0100

    Próby z warunkami

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Bartek!");
+﻿var name = "Ewa";
+var sex = 'f';
+var age = 30;
+
+if (sex == 'f' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == 'm' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
